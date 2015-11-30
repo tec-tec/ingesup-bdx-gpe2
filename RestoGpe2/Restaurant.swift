648ca977ba98ee82e95bce982a567259a11d17a1
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2015-11-30 16:02:53 +0100

    Added a Restaurant structure to modelize restaurants

---
+++ RestoGpe2/Restaurant.swift
@@ -0,0 +1,18 @@
+//
+//  Restaurant.swift
+//  RestoGpe2
+//
+//  Created by Ludovic Ollagnier on 30/11/2015.
+//  Copyright © 2015 Tec-Tec. All rights reserved.
+//
+
+import UIKit
+
+struct Restaurant {
+
+    let name: String
+    let address: String
+    var comment: String
+    var alreayVisited: Bool
+    var grade: Int?
+}
