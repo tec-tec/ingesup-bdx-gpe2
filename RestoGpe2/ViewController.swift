8e940530e5ad3c0be5837d096f51a29868c5f27a
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2015-11-30 15:59:52 +0100

    Added actions and outlets

--- RestoGpe2/ViewController.swift
+++ RestoGpe2/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var commentTextView: UITextView!
+    @IBOutlet weak var alreadyVisitedSwitch: UISwitch!
+    @IBOutlet weak var gradeSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+
+        view.backgroundColor = UIColor.rouseColor()    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func saveRestaurant(sender: AnyObject) {
 
+        print(nameTextfield.text!)
+        print(addressTextField.text!)
+        print(commentTextView.text)
+        print(alreadyVisitedSwitch.on)
+        print(gradeSlider.value)
+    }
+
+    @IBAction func switchValueChanged(sender: UISwitch) {
+
+        gradeSlider.enabled = sender.on
+//        gradeSlider.hidden = sender.off
+    }
+    @IBAction func gradeChanged(sender: AnyObject) {
+    }
 }
-
