d39c540ca183bfc5b45f1ac68002bee7f77dc146
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2015-12-15 08:50:40 +0100

    Added a basic tableview

---
+++ RestoGpe2/RestaurantsTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  RestaurantsTableViewController.swift
+//  RestoGpe2
+//
+//  Created by Ludovic Ollagnier on 30/11/2015.
+//  Copyright © 2015 Tec-Tec. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantsTableViewController: UITableViewController {
+
+    let restoManager = RestaurantManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return restoManager.allRestaurants.count
+    }
+
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        // Comment this an uncomment below for the customCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("RestoCell", forIndexPath: indexPath) as! UITableViewCell
+        let resto = restoManager.allRestaurants[indexPath.row]
+        cell.detailTextLabel?.text = resto.address
+
+//Uncomment for custom cell
+//        let cell = tableView.dequeueReusableCellWithIdentifier("RestoCell", forIndexPath: indexPath) as! CustomCell
+//        cell.aSwitch.setOn(false, animated: false)
+
+        return cell
+    }
+
+}
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet weak var aSwitch: UISwitch!
+
+}
