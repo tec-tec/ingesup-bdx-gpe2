aedd6bdab2f418322a72be662c6094f7af52076c
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2015-12-15 17:50:58 +0100

    Autolayouting the form

--- RestoGpe2/ViewController.swift
+++ RestoGpe2/ViewController.swift
@@ -22,8 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        view.backgroundColor = UIColor.rouseColor()    }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
